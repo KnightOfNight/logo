16128129b72bda6c8d2edc6337ecbd65e49f50c8
Cheyenne T. Greatorex <ctg@bluestatedigital.com>
2014-04-02 12:35:27 -0400

    Code cleanup and further revisions to the creation of the desktop wallpaper.

--- greatorex.php
+++ greatorex.php
@@ -4,34 +4,35 @@
 
 require("logo.php");
 
-logo( 5001, "chey.png", [0, 0, 0] );
-logo( 5001, "sharon.png", [0xE3, 0xA6, 0xEC] );
+logo(5001, "chey.png", [0, 0, 0]);
+logo(5001, "sharon.png", [0xE3, 0xA6, 0xEC]);
 
 
 
 $im_x = 3200;
 $im_y = 1800;
 
-$logo_x = 1000;
-$logo_y = 1000;
+$logo_x = 1200;
+$logo_y = 1200;
 
-$img = imagecreate( $im_x, $im_y );
+$img = imagecreate($im_x, $im_y);
 
-$gray = imagecolorallocate( $img, 128, 128, 128 );
+$gray = imagecolorallocate($img, 128, 128, 128);
 
-# $lilac = imagecolorallocate( $img, 0xE3, 0xA6, 0xEC );
+# $lilac = imagecolorallocate($img, 0xE3, 0xA6, 0xEC);
 
-imagefill( $img, 0, 0, $gray );
+imagefill($img, 0, 0, $gray);
 
 $logo = imagecreatefrompng("chey.png");
 
-imagecopyresampled( $img, $logo,
+imagecopyresampled($img, $logo,
 	($im_x / 2) - ($logo_x / 2), ($im_y / 2) - ($logo_y / 2),
 	0, 0,
 	$logo_x, $logo_y,
-	imagesx($logo), imagesy($logo) );
+	imagesx($logo), imagesy($logo)
+);
 
-imagepng( $img, "desktop(hires).png", 0);
+imagepng($img, "desktop.png", 0);
 
 exit(0);
 
